7f0de56df8fc0afccba4ad48844a463932667378
Ori Shoshan <ori@otterize.com>
2024-03-09 23:28:47 +0100

    Update README, remove otterize values

--- otterize-aws/main.tf
+++ otterize-aws/main.tf
@@ -1,16 +1,9 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
-
-provider "kubernetes" {
-  config_path = "~/.kube/config"
+module "otterize-aws-iam" {
+  #source  = "otterize/otterize-aws-iam/aws"
+  source = "../../terraform-otterize-aws-iam-rolesanywhere"
+  #version = "1.0.0"
+  cert_manager_trust_anchor_ca_secret_name = "cert-manager-spiffe"
+  cert_manager_namespace = "cert-manager"
+  aws_region = "us-west-2"
+  cluster_name = "otterize-csi-spiffe-demo"
 }
