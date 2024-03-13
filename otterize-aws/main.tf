b9ebebdaa01e5f4565555798cb6e8b82762acb55
Ori Shoshan <ori@otterize.com>
2024-03-13 19:05:05 +0100

    Fix aws region

--- otterize-aws/main.tf
+++ otterize-aws/main.tf
@@ -3,6 +3,6 @@ module "otterize-aws-iam" {
   version = "1.0.0"
   cert_manager_trust_anchor_ca_secret_name = "cert-manager-spiffe"
   cert_manager_namespace = "cert-manager"
-  aws_region = "us-west-2"
+  aws_region = "eu-west-2"
   cluster_name = "otterize-csi-spiffe-demo"
 }
