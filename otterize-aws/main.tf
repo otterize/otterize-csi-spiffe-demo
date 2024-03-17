b3d320fa70e62f07420fb3c6bd37a8a40af1c888
Ori Shoshan <ori@otterize.com>
2024-03-17 10:46:42 +0100

    Update main.tf

--- otterize-aws/main.tf
+++ otterize-aws/main.tf
@@ -1,6 +1,6 @@
 module "otterize-aws-iam" {
   source  = "otterize/otterize-aws-iam-rolesanywhere/aws"
-  version = "1.0.1"
+  version = "~> 1.0.2"
   cert_manager_trust_anchor_ca_secret_name = "cert-manager-spiffe"
   cert_manager_namespace = "cert-manager"
   aws_region = "eu-west-2"
